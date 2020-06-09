0d19849473c735cf460d8749cc72659531d59a4e
Nyasha Vision <nmawungwe@gmail.com>
2020-06-09 12:03:49 +0200

    partial login

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request, redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+
+
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 print(os.environ['APP_SETTINGS'])
+
+#app secret key
+app.secret_key = 'your secret key'
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -22,10 +27,37 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+# Intialize MySQL
 
-@app.route("/")
-def index():
-    return "Project 1: TODO"
+
+@app.route("/", methods=['GET', 'POST'])
+def login():
+    # Output message if something goes wrong...
+    msg = ''
+        # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        rows = db.execute("SELECT * FROM accounts WHERE username = :username",
+                            {"username": username})
+        
+        account = rows.fetchone()
+                # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # Redirect to home page
+            return 'Logged in successfully!'
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    return render_template('index.html', msg=msg)
+
+
 
 if __name__ == '__main__':
     app.run()
