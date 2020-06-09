28837b4c7eec40e88a7f41252a8ecab78344dff7
Nyasha Vision <nmawungwe@gmail.com>
2020-06-09 14:07:50 +0200

    added modules

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import os
 
-from flask import Flask, session, render_template, request, redirect, url_for
+from flask import Flask, session, request, jsonify, render_template, flash, redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
+import requests
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -32,30 +34,25 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/", methods=['GET', 'POST'])
 def login():
-    # Output message if something goes wrong...
-    msg = ''
-        # Check if "username" and "password" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        # Create variables for easy access
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        # Check if account exists using MySQL
-        rows = db.execute("SELECT * FROM accounts WHERE username = :username",
+        error = None
+        users = db.execute("SELECT * FROM accounts WHERE username = :username",
                             {"username": username})
-        
-        account = rows.fetchone()
-                # If account exists in accounts table in out database
-        if account:
-            # Create session data, we can access this data in other routes
-            session['loggedin'] = True
-            session['id'] = account['id']
-            session['username'] = account['username']
-            # Redirect to home page
-            return 'Logged in successfully!'
-        else:
-            # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!'
-    return render_template('index.html', msg=msg)
+        user = users.fetchone()
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+        if error is None:
+            # store the user id in a new session and return to the index
+            session.clear()
+            session['user_id'] = user['id']
+            return 'Logged in!'
+        flash(error)
+    return render_template('index.html') 
+ 
 
 
 
