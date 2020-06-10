fdd465561238998125114b774ed6ed9994f399a5
Nyasha Vision <nmawungwe@gmail.com>
2020-06-10 21:54:44 +0200

    added login with encryption

--- app.py
+++ app.py
@@ -40,16 +40,56 @@ def index():
 def profile():
     return render_template('profile.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('login.html')
+    """ Log user in """
+
+    # Forget any user_id
+    session.clear()
+
+    username = request.form.get("username")
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return 'Enter username'
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return 'Enter password'
+
+        # Query database for username (http://zetcode.com/db/sqlalchemy/rawsql/)
+        # https://docs.sqlalchemy.org/en/latest/core/connections.html#sqlalchemy.engine.ResultProxy
+        result = db.execute("SELECT * FROM users WHERE username = :username",
+                            {"username": username}).fetchone()
+        
+
+        # Ensure username exists and password is correct
+        if result == None or not check_password_hash(result[2], request.form.get("password")):
+            return 'invalid username and/or password'
+
+        # Remember which user has logged in
+        session["user_id"] = result[0]
+        session["user_name"] = result[1]
+
+        # Redirect user to home page
+        return redirect(url_for('profile'))
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+    
+
+ 
 
 @app.route('/signup', methods=['GET','POST'])
-def signup_post():
+def signup():
 # User reached route via POST (as by submitting a form via POST)
     if request.method == 'POST':
         # username = request.form.get('username')
-        password_hash = request.form.get(generate_password_hash('password'))
+
             
         user = db.execute("SELECT * FROM users WHERE username = :username",
                             {"username":request.form.get("username")}).fetchone()
@@ -57,8 +97,10 @@ def signup_post():
         if user:
             flash('Username already exists')
             return render_template('signup.html')
-                
-        db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
+
+        password = request.form.get("password")
+        password_hash = generate_password_hash(password, method='sha256', salt_length=8)       
+        db.execute("INSERT INTO users (username, passwords) VALUES (:username, :password)",
                                     {"username":request.form.get("username"), 
                                     "password":password_hash})
             # Commit changes to database
