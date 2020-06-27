20b913b33f6417bed9336092e23f4ea12f51a251
Nyasha Vision <nmawungwe@gmail.com>
2020-06-28 00:01:50 +0200

    added books to database

--- import.py
+++ import.py
@@ -1,16 +1,13 @@
 import os
 
-from flask import Flask, render_template, request
+import csv
+from flask import Flask
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-
-
-# checking which developing configuration? 
-print(os.environ['APP_SETTINGS'])
 
   # Tell Flask what SQLAlchemy database to use.
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
@@ -22,11 +19,15 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-@app.route('/')
-def index():
-    return('import is working')
-    
+
+f = open("books.csv")
+reader = csv.reader(f)
+for isbn, title, author, year in reader:
+      db.execute("INSERT INTO Books (isbn, title, author, publication_year) VALUES (:isbn, :title, :author, :publication_year)",
+                  {"isbn": isbn, "title":title , "author":author, "publication_year":year}) # substitute values from CSV line into SQL command, as per this dict
+      print(f"Added book: isbn:{isbn} title:{title} author:{author} publication_year:{year}")
+      
+      db.commit() 
+  
 
 
-if __name__ == "__main__":
-    app.run()
